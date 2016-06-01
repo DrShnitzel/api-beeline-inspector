d564f70bc0f2da1250d7f4494bd2a61f19fb85ab
Aleksei Zaitcev <a.zaitsev@tarifer.ru>
2016-06-01 13:06:20 +0300

    Remove technical services from responce

--- app/collectors/services_collector.rb
+++ app/collectors/services_collector.rb
@@ -7,7 +7,7 @@ class ServicesCollector < BaseCollector
 
   def prepare_response(resp)
     resp['services'].each_with_object([]) do |item, acc|
-      acc << prepare_response_item(item)
+      acc << prepare_response_item(item) if not_technical?(item)
     end
   end
 
@@ -18,5 +18,9 @@ class ServicesCollector < BaseCollector
       payment_period: payment_period(item),
       removable: item['removeInd']
     }
+  end
+
+  def not_technical?(item)
+    item['viewInd'] == 'Y'
   end
 end
